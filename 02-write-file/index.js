87fb1e4b203f539f52ddb72be5d19e15e22e3d77
Katia <katia.zakharina@gmail.com>
2021-11-06 00:22:02 +0300

    feat: add 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filepath = path.join(__dirname, 'output.txt');
+
+const readlineSteam = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Enter some text\n'
+});
+
+readlineSteam.prompt();
+
+readlineSteam.on('line', (line) => {
+  switch (line.trim()) {
+  case 'exit':
+    readlineSteam.close();
+    break;
+  default:
+    fs.appendFile( filepath, line.trim()+'\n', err => {
+      if (err) console.error(err);
+    });
+    break;
+  }
+}).on('close', () => {
+  console.log('Have a great day!');
+});
